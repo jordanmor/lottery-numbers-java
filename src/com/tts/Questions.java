0fbb2c146c785ea6cc878bdd0a1b344c01dfa00b
Jordan Mor <jordanmor@gmail.com>
2019-05-28 16:21:20 -0400

    Validate number input ranges

--- src/com/tts/Questions.java
+++ src/com/tts/Questions.java
@@ -9,16 +9,34 @@ public class Questions {
 	
 	public static void askName() {
 		System.out.println("Please enter your name: ");
+		while(!scanner.hasNext("[A-Za-z]+")) {
+			scanner.next();
+			System.out.println("Please enter a valid name.");
+		}
 		System.out.println("Hello " + scanner.next());
 	}
 	
 	public static void askMainQuestions() {
 		String favoritePet = questionStringOutput("What is your favorite pet?");
-		String actorName = questionStringOutput("What is the first name of the your favorite actor or actress?");
+		
 		int petAge = questionIntegerOutput("What is the age of your pet?");
-		int luckyNumber = questionIntegerOutput("What is your lucky number?");
-		int carYear = questionIntegerOutput("What is two-digit model year of their car?");
-		int chosenNumber = questionIntegerOutput("Enter a random number between 1 and 50.");
+		
+		int luckyNumber = questionIntegerOutput("What is your lucky number? (Please choose a number between 1 and 100)");
+		while(luckyNumber < 1 || luckyNumber > 99) {
+			luckyNumber = questionIntegerOutput("That number was outside the permitted range. Please enter your lucky number (between 1 and 100)");
+		}
+		
+		int carYear = questionIntegerOutput("What is the two-digit model year of your car?");
+		while(carYear < 0 || carYear > 99) {
+			carYear = questionIntegerOutput("Please enter a valid two-digit model year of your car");
+		}
+		
+		String actorName = questionStringOutput("What is the first name of the your favorite actor or actress?");
+		
+		int chosenNumber = questionIntegerOutput("Enter a random number between 1 and 50");
+		while(chosenNumber < 1 || chosenNumber > 50) {
+			chosenNumber = questionIntegerOutput("That number was outside the permitted range. Please enter a random number between 1 and 50");
+		}
 		
 		LotteryNumbers.generate(favoritePet, petAge, luckyNumber, carYear, actorName, chosenNumber);
 	}
@@ -29,7 +47,7 @@ public class Questions {
 	}	
 	
 	public static boolean repeat() {
-		System.out.println("Would like to answer questions to generate another set of numbers? (yes,no):");
+		System.out.println("Would you like to answer questions to generate another set of numbers? (yes,no):");
 		boolean response = createBooleanResponse(scanner.next());
 		if(response == false) {
 			System.out.println("Thank you for participating in this survey.");
