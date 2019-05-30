5e687e4884ad70093039ba236b2e5ae0fa519048
Jordan Mor <jordanmor@gmail.com>
2019-05-29 20:15:19 -0400

    Eliminate all duplicate lottery numbers

--- src/com/tts/LotteryNumbers.java
+++ src/com/tts/LotteryNumbers.java
@@ -4,10 +4,9 @@ public class LotteryNumbers {
 	// Generate 5 lottery numbers and one magic ball using valid user input from the 6 questions
 	public static void generate(String favoritePet, int petAge, int luckyNumber ,int carYear, String actorName, int chosenNumber) {
 
-		System.out.println("Chosen numbers: " + favoritePet + ", " + petAge + ", " + luckyNumber + ", " + carYear + ", " + actorName + ", " + chosenNumber);
 		// Magic Ball
 		// Generate number by multiplying lucky number by random number with range of 1 - 12;
-		int magicBall = luckyNumber * (int)(Math.random() * 12);
+		int magicBall = luckyNumber * (int)(Math.random() * 12) + 1; 
 		
 		// Initialize array with a length of six for lottery numbers
 		int lotteryArray[] = new int[5];
@@ -22,7 +21,7 @@ public class LotteryNumbers {
 		
 		// Lottery Number 3
 		// Use the random number between 1 and 50, subtracting by a random number with a range of 1 - 15.
-		lotteryArray[2] = chosenNumber - (int)(Math.random() * 15);
+		lotteryArray[2] = chosenNumber - (int)(Math.random() * 15) + 1;
 		
 		// Lottery Number 4
 		// Convert the last letter of their favorite actor/actress to an integer and use that value
@@ -30,7 +29,7 @@ public class LotteryNumbers {
 		
 		// Lottery Number 5
 		// Pet age + lucky number minus a random number with a range of 1 - 18.
-		lotteryArray[4] = petAge + luckyNumber - (int)(Math.random() * 18);
+		lotteryArray[4] = petAge + luckyNumber - (int)(Math.random() * 18) + 1;
 				
 		// Loop through lottery array and make sure each number is less than max value of 65
 		for(int x = 0; x < lotteryArray.length; x++) {
@@ -40,8 +39,10 @@ public class LotteryNumbers {
 		// Make sure magic ball number is less than max value of 75
 		magicBall = validateNumberValue(magicBall, 75);
 		
+		int lotteryArrayWithoutDuplicates[] = eliminateDuplicateNums(lotteryArray);
+		
 		// Print out lottery numbers and magic ball number to user
-		System.out.println("Lottery numbers: " + lotteryArray[0] + ", " + lotteryArray[1] + ", " + lotteryArray[2] + ", " + lotteryArray[3] + ", " + lotteryArray[4] + " Magic Ball: " + magicBall);
+		printLotteryNumbers(lotteryArrayWithoutDuplicates, magicBall);
 	}
 	
 	// Function checks whether number is above max value
@@ -52,5 +53,35 @@ public class LotteryNumbers {
 			input -= maxValue;
 		}
 		return input;
+	}
+	
+	// Function checks array of lottery numbers for duplicate numbers
+	// Any duplicate number is changed to a random number from 1 - 65
+	// Loop continues until all duplicate numbers are removed and replaced with unique numbers
+	private static int[] eliminateDuplicateNums(int[] array) {
+
+		boolean duplicates;
+			
+		for(int x = 0; x < array.length; x++) {
+			array[x] = (int)(Math.random() * 65);
+		}		
+
+		do {
+			duplicates = false;
+			for(int x = 0; x < array.length; x++) {
+				for(int y = x + 1; y < array.length; y++) {
+					if(array[x] == array[y]) {
+						array[x] = (int)(Math.random() * 65);
+					}
+				}
+			}
+		} while(duplicates);
+				
+		return array;
+	}
+	
+	// Print out lottery numbers and magic ball number to user
+	private static void printLotteryNumbers(int[] array, int magicBall) {
+		System.out.println("Lottery numbers: " + array[0] + ", " + array[1] + ", " + array[2] + ", " + array[3] + ", " + array[4] + " Magic Ball: " + magicBall);
 	}
 }
